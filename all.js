2f926988790930c35927a8c8f69c9f51d1910612
joy <aa012003@gmail.com>
2024-01-15 18:11:45 +0800

    最終版

--- all.js
+++ all.js
@@ -1,45 +1,44 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 createApp({
-  data() {
-    return {
-      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
-      apiPath: 'joychiang',
-      products: [],
-      tempProduct: {},
+  data(){
+    return{
+      apiUrl:'https://vue3-course-api.hexschool.io/v2',
+      apiPath:'joychiang',
+      products:[],
+      showProduct:{},
     }
   },
   methods: {
-    checkAdmin() {
-      const url = `${this.apiUrl}/api/user/check`;
+    checkAdmin(){
+      const url=`${this.apiUrl}/api/user/check`;
       axios.post(url)
-        .then(() => {
-          this.getData();
-        })
-        .catch((err) => {
-          alert(err.response.data.message)
-          window.location = 'login.html';
-        })
+      .then(()=>{
+        this.getProducts();
+        console.log("驗證成功");
+      })
+      .catch((err)=>{
+        alert(err.response.data.message)
+        window.location='login.html'
+        ;
+      })
     },
-    getData() {
-      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+    getProducts(){
+      const url = `${this.apiUrl}/api/joychiang/admin/products/all`;
       axios.get(url)
-        .then((response) => {
-          this.products = response.data.products;
-        })
-        .catch((err) => {
-          alert(err.response.data.message);
-        })
-    },
-    openProduct(item) {
-      this.tempProduct = item;
+      .then((res)=>{
+        this.products=res.data.products
+      })
+      .catch((err)=>{
+        alert(err.response.data.message);
+      })
     }
+
   },
   mounted() {
-    // 取出 Token
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)joyToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
     axios.defaults.headers.common.Authorization = token;
 
     this.checkAdmin()
-  }
+  },
 }).mount('#app');
